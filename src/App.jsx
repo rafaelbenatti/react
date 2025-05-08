41eb1ce434416f0e100dbf55030b5aea0215e613
Rafael Benatti <rafaelbenatti3@gmail.com>
2025-05-08 11:40:58 -0300

    'v1'

---
+++ src/App.jsx
@@ -0,0 +1,75 @@
+import { useEffect, useState } from "react";
+import AddTask from "./components/AddTask";
+import Tasks from "./components/Tasks";
+import { v4 } from "uuid";
+import Title from "./components/Title_Gerenciador";
+
+function App() {
+  const [tasks, setTasks] = useState(
+    JSON.parse(localStorage.getItem("tasks")) || []
+  );
+
+  // useEffect(() => {
+  //   async function fetchTasks() {
+  //     // chamar api
+  //     const response = await fetch(
+  //       "https://jsonplaceholder.typicode.com/todos?_limit=10",
+  //       { method: "GET" }
+  //     );
+
+  //     // pegar dados que ela retorna
+  //     const data = await response.json();
+  //     console.log(data);
+
+  //     // armazenar/persistir dados no state
+  //     setTasks(data);
+  //   }
+  //   // fetchTasks();
+  // }, []);
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  function onTaskClick(taskId) {
+    const newTasks = tasks.map((task) => {
+      if (task.id == taskId) {
+        return { ...task, isCompleted: !task.isCompleted };
+      }
+
+      return task;
+    });
+    setTasks(newTasks);
+  }
+
+  function onDeleteTaskClick(taskId) {
+    const newTaks = tasks.filter((task) => task.id != taskId);
+    setTasks(newTaks);
+  }
+
+  function onAddTaskSubmit(title, description) {
+    const newTask = {
+      id: v4(),
+      title,
+      description,
+      isCompleted: false,
+    };
+    setTasks([...tasks, newTask]);
+  }
+
+  return (
+    <div className="bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px] space-y-4">
+        <Title>Gerenciador de Tarefas</Title>
+        <AddTask onAddTaskSubmit={onAddTaskSubmit} />
+        <Tasks
+          tasks={tasks}
+          onTaskClick={onTaskClick}
+          onDeleteTaskClick={onDeleteTaskClick}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
