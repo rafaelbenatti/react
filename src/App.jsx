4d46b0f1296b213dadc0a1dc7915f81f0209fd99
Rafael Benatti <rafaelbenatti3@gmail.com>
2025-05-08 12:07:49 -0300

    App.js min-h-screen

--- src/App.jsx
+++ src/App.jsx
@@ -58,7 +58,7 @@ function App() {
   }
 
   return (
-    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+    <div className="w-screen min-h-screen bg-slate-500 flex justify-center p-6">
       <div className="w-[500px] space-y-4">
         <Title>Gerenciador de Tarefas</Title>
         <AddTask onAddTaskSubmit={onAddTaskSubmit} />
